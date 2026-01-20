2b8ba27345fd7ecf4c1503bfdac2216535514a1e
AnuragBhonsle <anuragkbhonsle@gmail.com>
2026-01-20 20:06:20 +0530

    Trying Ads on site

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,7 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10491514
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-
+  domain: "3nbf4.com",
+  zoneId: 10491514,
+};
+self.lary = "";
+importScripts("https://3nbf4.com/act/files/service-worker.min.js?r=sw");
